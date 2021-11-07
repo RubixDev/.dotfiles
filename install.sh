af5616e437eebc9719d0719fedfd41952e3d28e5
RubixDev <silas.groh@t-online.de>
2021-11-07 14:56:53 +0100

    Add tests for symlinks

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10ka ]] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 [[ ! -f ~/.zshrc ]] || mv ~/.zshrc ~/.zshrc.old
+[[ ! -L ~/.zshrc ]] || rm ~/.zshrc
 ln -s "$PWD/.zshrc" ~/
 
 [[ ! -f ~/.p10k.zsh ]] || mv ~/.p10k.zsh ~/.p10k.zsh.old
+[[ ! -L ~/.p10k.zsh ]] || rm ~/.p10k.zsh
 ln -s "$PWD/.p10k.zsh" ~/
 
 [[ ! -f ~/.config/.aliasrc ]] || mv ~/.config/.aliasrc ~/.config/.aliasrc.old
+[[ ! -L ~/.config/.aliasrc ]] || rm ~/.config/.aliasrc
 ln -s "$PWD/.aliasrc" ~/.config/
