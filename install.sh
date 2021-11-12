8bf313577ab0a17f33576c335043bf2618ba0c53
RubixDev <silas.groh@t-online.de>
2021-11-12 16:42:27 +0100

    Add terminator config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_file () {
+    mkdir -p "$HOME/$(dirname "$1")"
     [[ ! -f ~/"$1" ]] || mv ~/"$1" ~/"$1".old
     [[ ! -L ~/"$1" ]] || rm ~/"$1"
     ln -s "$PWD/$1" "$HOME/$1"
@@ -15,3 +16,4 @@ install_file .zshrc
 install_file .p10k.zsh
 install_file .config/.aliasrc.zsh
 install_file .tmux.conf
+install_file .config/terminator/config
