8b9474fb2412311b923854e8570dc9b4e970188b
RubixDev <silas.groh@t-online.de>
2022-05-27 20:24:31 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,14 +1,95 @@
-#!/bin/bash
+#!/bin/sh
 
+[ "$(basename "$PWD")" = .dotfiles ] || {
+    echo "Please run this script from your .dotfiles project root"
+    exit 1
+}
+
+########## Dependency Installation ##########
+# TODO: ask for installation
+
+if [ -n "$WAYLAND_DISPLAY" ] || [ -n "$DISPLAY" ]; then
+    is_desktop=true
+fi
+
+if command -v uname > /dev/null && [ "$(uname -o)" = "Android" ]; then
+    true
+else
+    . /etc/os-release
+    case "$ID" in
+        "arch")
+            if command -v paru > /dev/null; then
+                aur=paru
+            elif command -v yay > /dev/null; then
+                aur=yay
+            else
+                echo "Neither paru nor yay is installed on your system, but required for AUR packages"
+                exit 1
+            fi
+
+            command -v rustup > /dev/null && has_rustup=true
+
+            $aur -Sy --needed fd ripgrep proximity-sort neovim zsh rustup fzf git curl wget shellcheck \
+                pfetch neovim-plug nodejs yarn || exit 2
+            [ "$is_desktop" = true ] && $aur -S --needed polybar sway-launcher-desktop bspwm sxhkd dunst \
+                alacritty picom nitrogen numlockx slock neovim-remote ttf-meslo-nerd-font-powerlevel10k
+
+            [ "$has_rustup" = true ] || rustup default stable
+            ;;
+        "debian")
+            sudo apt install zsh fzf git curl wget shellcheck nodejs npm || exit 2
+            [ "$is_desktop" = true ] && sudo apt install bspwm sxhkd polybar dunst picom nitrogen numlockx \
+                suckless-tools
+
+            sudo npm install -g yarn || exit 2
+
+            if ! command -v rustup > /dev/null; then
+                curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+                rustup default stable
+            fi
+
+            cargo install fd-find ripgrep proximity-sort || exit 2
+            [ "$is_desktop" = true ] && cargo install alacritty
+
+            if ! command -v nvim > /dev/null; then
+                wget 'https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb' || exit 2
+                sudo apt install ./nvim-linux64.deb
+                rm ./nvim-linux64.deb
+            fi
+
+            sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+            if ! command -v pfetch > /dev/null; then
+                wget 'https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch'
+                sudo cp ./pfetch /usr/local/bin/pfetch
+                sudo chmod +x /usr/local/bin/pfetch
+                rm ./pfetch
+            fi
+            ;;
+        *)
+            echo "Automatic dependency installation is not supported for this distribution"
+            exit 3
+    esac
+fi
+
+# oh-my-zsh
+[ -e ~/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+[ -e "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] || git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+[ -e "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search" ] || git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
+[ -e "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+[ -e "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode" ] || git clone https://github.com/jeffreytse/zsh-vi-mode "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode"
+
+########### dotfiles Installation ###########
 install_file () {
     mkdir -p "$HOME/$(dirname "$1")"
-    [[ ! -f ~/"$1" ]] || mv ~/"$1" ~/"$1".old
-    [[ ! -L ~/"$1" ]] || rm ~/"$1"
+    [ ! -L ~/"$1" ] || rm ~/"$1"
+    [ ! -e ~/"$1" ] || mv ~/"$1" ~/"$1".old
     ln -s "$PWD/$1" "$HOME/$1"
 }
 
 # Install powerlevel10k theme, if not yet present
-[[ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]] || {
+[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ] || {
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 }
 
