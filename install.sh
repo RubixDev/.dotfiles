c3c72807af5d9c87d3a4629447454f23faff2e97
RubixDev <silas.groh@t-online.de>
2022-05-28 12:20:22 +0200

    Clean up after debian alacritty install

--- install.sh
+++ install.sh
@@ -61,13 +61,15 @@ install_debian () {
         git pull
         cargo build --release
         infocmp alacritty > /dev/null || sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-        sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+        sudo cp "${CARGO_TARGET_DIR:-target}"/release/alacritty /usr/local/bin
         sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
         sudo desktop-file-install extra/linux/Alacritty.desktop
         sudo update-desktop-database
         sudo mkdir -p /usr/local/share/man/man1
         gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
         gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+        cd .. || exit 2
+        rm -rf alacritty
     fi
 
     if ! command -v nvim > /dev/null; then
