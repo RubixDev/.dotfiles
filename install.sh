897232f30924b99aaa217317955d8bd042659dac
RubixDev <silas.groh@t-online.de>
2022-06-05 21:24:28 +0200

    Remove terminator config

--- install.sh
+++ install.sh
@@ -229,7 +229,6 @@ install_file .config/nvim/init.vim
 install_file .config/paru/paru.conf
 install_file .config/npm/npmrc
 if [ "$is_desktop" = true ]; then
-    install_file .config/terminator/config
     install_file .config/alacritty/alacritty.yml
     install_file .config/i3/config
     install_file .config/bspwm/bspwmrc
