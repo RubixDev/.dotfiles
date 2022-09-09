7b0b5dd07b7cb61d749787cbdd27ddf00c7b0770
RubixDev <silas.groh@t-online.de>
2022-09-09 20:30:11 +0200

    Vim: Automatically run PackerCompile on plugin change

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,10 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function (use)
     -- Theme
     use 'rakr/vim-one'
