a638d86709fc0ebdb869d691f3d6a1a2013770dc
RubixDev <silas.groh@t-online.de>
2022-09-22 18:34:51 +0200

    install.sh: Remove stylua from automatic installation

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ install_android () {
 
     pkg update -y
     pkg install -y ripgrep fd neovim zsh rust fzf git onefetch curl wget shellcheck \
-        nodejs exa bat tmux lf python stylua || exit 2
+        nodejs exa bat tmux lf python || exit 2
     cargo install pixterm || exit 2
 
     if [ "$(basename "$SHELL")" != "zsh" ]; then
@@ -138,7 +138,7 @@ install_arch () {
     fi
 
     $aur -Sy --needed --noconfirm base-devel fd ripgrep neovim zsh rustup fzf git curl wget \
-        shellcheck pfetch-git nodejs npm exa bat tmux onefetch lf go stylua \
+        shellcheck pfetch-git nodejs npm exa bat tmux onefetch lf go \
         || [ "$is_root" = true ] || exit 2
     rustup default > /dev/null 2>&1 || { rustup default stable || exit 2; }
     $aur -S --needed --noconfirm pixterm-rust autojump-rs pixfetch \
@@ -200,7 +200,7 @@ install_debian () {
     fi
 
     rustup default > /dev/null 2>&1 || { rustup default stable || exit 2; }
-    cargo install fd-find ripgrep onefetch pixterm autojump stylua tree-sitter-cli bat || exit 2
+    cargo install fd-find ripgrep onefetch pixterm autojump tree-sitter-cli bat || exit 2
 
     if ! command -v nvim > /dev/null; then
         wget 'https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb' || exit 2
