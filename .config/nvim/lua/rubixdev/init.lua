f97d2e14a7b2fbb67e7abdcfbdaa348f33393969
RubixDev <silas.groh@t-online.de>
2022-12-04 18:33:00 +0100

    alias/nvim: Add `shareclip` and `loadclip` aliases

--- .config/nvim/lua/rubixdev/init.lua
+++ .config/nvim/lua/rubixdev/init.lua
@@ -2,3 +2,6 @@ _G.is_android = vim.fn.executable('uname') == 1 and vim.fn.system('uname -o') ==
 
 require('rubixdev.packer')
 require('rubixdev.mappings')
+
+vim.api.nvim_create_user_command('Shareclip', function() io.popen('zsh -i -c "shareclip"') end, {})
+vim.api.nvim_create_user_command('Loadclip', function() io.popen('zsh -i -c "loadclip"') end, {})
